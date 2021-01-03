0e3f9f98c3394d001fc618df1c6f950eb2145c39
provlhmatikos <orfeas.zografos@gmail.com>
2021-01-03 22:09:16 +0200

    Add setters-getters to Point Entity Class

--- back-end/Rest/src/main/java/Hardeng/Rest/models/Point.java
+++ back-end/Rest/src/main/java/Hardeng/Rest/models/Point.java
@@ -1,5 +1,8 @@
 package Hardeng.Rest.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 // import javax.persistence.ForeignKey;
@@ -16,26 +19,72 @@ public class Point {
     private Integer id;
 
     @Column(nullable = false)
-    private Float longitude;
+    private Double longitude;
 
     @Column(nullable = false)
-    private Float latitude;
+    private Double latitude;
 
     @ManyToOne
     private Integer operatorId;
-
-    /** 
-     * Stores the current condition of the charging point.
-     * <br></br>
-     * e.g.: 0->OK, -1->DOWN, 1->UNDER_REPAIRS, 2->DUE_CHECK, 3->UNDER_CONSTRUCTION
-     */
-    @Column(nullable = false, length = 8)
-    private Integer condition; 
 
     @Column(nullable = false)
     private Integer maxOutput;
 
     @Column(nullable = false)
     private Boolean isOccupied;
+    
+    @Column(nullable = false, length = 8)
+    private Integer condition; 
+    
+    public enum Condition {
+        OPERATIONAL(0), DOWN(-1), UNDER_REPAIRS(1), NEEDS_CHECK(2), UNDER_CONSTRUCTION(3);
+        public final Integer value;
+        private static final Map<Integer, Condition> BY_LABEL = new HashMap<>();
+        private Condition(Integer value) {this.value = value;}
+        /** This runs once and populates the lookup table from Integer to Condition */
+        static {for (Condition c: values()) {BY_LABEL.put(c.value, c);}}
+        /** Translates code to Condition type 
+         * @param code The value to be translated 
+         * @return The corresponding Condition 
+         */
+        public static Condition meaningOfCode(Integer code) {return BY_LABEL.get(code);}
+    }
 
+    Point() {}
+    /**
+     * Point entity constructor.
+     * @param longitude self-explanatory
+     * @param latitude self-explanatory
+     * @param operatorId The Id of the Corresponding Point operator
+     * @param maxOutput The max power output this point is capable of
+     * @param condition The current operational condition/status of the point
+     * @param isOccupied Stores whether it's being used at the moment.
+     */
+    Point(Double longitude, Double latitude, Integer operatorId , Integer maxOutput, Integer condition,  Boolean isOccupied) {
+        this.longitude = longitude; this.latitude = latitude; this.operatorId = operatorId; this.maxOutput = maxOutput;
+        this.condition = condition; this.isOccupied = isOccupied;
+    }
+    /** 
+     * Overload of the {@link #Point(Double, Double, Integer, Integer, Integer, Boolean)} method with default {@code condition=0}(OPERATIONAL)
+     * and {@code isOccupied=false}
+     */
+    Point(Double longitude, Double latitude, Integer operatorId, Integer maxOutput) {
+        this.longitude = longitude; this.latitude = latitude; this.operatorId = operatorId; this.maxOutput = maxOutput;
+        this.condition = 0; this.isOccupied = false;
+    }
+
+
+    public Integer getId() {return this.id;}
+    public Double getLatitude() {return this.latitude;}
+    public Double getLongitude() {return this.longitude;}
+    public Integer getMaxOutput() {return this.maxOutput;}
+    public Boolean isOccupied() {return this.isOccupied;}
+    public Condition getCondition() {return Condition.meaningOfCode(this.condition);}
+
+    public void setLatitude(Double newLatitude) {this.latitude = newLatitude;}
+    public void setLongitude(Double newLongitude) {this.longitude = newLongitude;}
+    public void setMaxOutput(Integer newMaxOutput) {this.maxOutput = newMaxOutput;}
+    public void setIsOccupied() {this.isOccupied = true;}
+    public void resetIsOccupied() {this.isOccupied = false;}
+    public void setCondition(Condition status) {this.condition = status.value;}
 }
