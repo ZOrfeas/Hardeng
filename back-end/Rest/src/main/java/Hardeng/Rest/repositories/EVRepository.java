705e9f0aea04af928627579b50c5ef0fac7d1f19
provlhmatikos <orfeas.zografos@gmail.com>
2021-01-07 18:31:35 +0200

    Cleanup after erroneous merge

--- back-end/Rest/src/main/java/Hardeng/Rest/repositories/EVRepository.java
+++
@@ -1,9 +0,0 @@
-package Hardeng.Rest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import Hardeng.Rest.models.EV;
-
-@Repository
-public interface EVRepository extends JpaRepository<EV, Integer> {}
