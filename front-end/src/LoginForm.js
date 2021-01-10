cbb5a6a2483a04b719c6c20b439b07ce285d8f76
Jason Chatzitheodorou <chatzitheodorou99@gmail.com>
2021-01-10 22:51:44 +0200

    Added login popup

--- front-end/src/LoginForm.js
+++ front-end/src/LoginForm.js
@@ -1,56 +1,76 @@
 import React from "react";
+import Popup from "reactjs-popup";
+import 'reactjs-popup/dist/index.css';
+import './LoginForm.css';
 
-class LoginForm extends React.Component{
-    constructor(){
-        super();
-        this.state = { 
-            username: '',
-            password: '',
-            error: null 
-        };
-        this.handleInput = this.handleInput.bind(this);
-    }
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      other: {},
+      error: null
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleInput(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({[name]: value});
-    }
+  handleInput(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  }
 
-    getName(){
-        return this.state.name;
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
 
-    render(){
-        return (
-            <div className="container">
-            <form onSubmit={(e) => e.preventDefault()}>
-                <div className="row">
-                    <div className="center input-field col s4">
-                        <input 
-                        name="username" 
-                        placeholder="Username"
-                        value={this.state.username}
-                        onChange={this.handleInput}
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="input-field col s4 center-block">
-                        <input 
-                        name="password" 
-                        type="password" 
-                        placeholder="Password"
-                        onChange={this.handleInput}
-                        />
-                    </div>
-                </div>
-                
-                <button className="center btn waves-effect waves-light"> Sign In </button>     
+  getName() {
+    return this.state.username;
+  }
+
+  render() {
+    return (
+      <div>
+        <Popup
+          trigger={<button className="btn right waves-effect waves-light"> Login </button>}
+          modal
+        >
+          <diV className="center-align">
+            <h5 > Use your information </h5>
+          </diV>
+          <div className="container">
+            <form className="addSpace" onSubmit={this.handleSubmit}>
+              <div className="input-field browser-default">
+                <input
+                  name="username"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleInput}
+                />
+              </div>
+              <div className="input-field input-outlined">
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={this.handleInput}
+                />
+              </div>
+              <div className="right-align addSpace">
+                <button
+                  className="btn waves-effect waves-light"
+                >
+                  Sign In
+              </button>
+              </div>
             </form>
-            </div>
-        )
-    }
+          </div>
+        </Popup>
+      </div>
+    )
+  }
 }
 
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 import ChargingExperience from './ChargingExperience';
+import LoginForm from './LoginForm';
 /* Imports */
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChargingExperience />
+    {/*<ChargingExperience />*/}
     {/*<StationMonitoring />*/}
     {/*<>*/}
+    <LoginForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
