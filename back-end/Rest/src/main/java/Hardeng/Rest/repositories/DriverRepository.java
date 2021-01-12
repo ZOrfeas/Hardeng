43ee5730c97af45e25b4975e709a06de0f93e098
provlhmatikos <orfeas.zografos@gmail.com>
2021-01-12 18:42:33 +0200

    To ka8aristria den einai dw

--- back-end/Rest/src/main/java/Hardeng/Rest/repositories/DriverRepository.java
+++ back-end/Rest/src/main/java/Hardeng/Rest/repositories/DriverRepository.java
@@ -13,14 +13,14 @@ public interface DriverRepository extends JpaRepository<Driver, Integer> {
     /**
      * Searches for a driver by username
      * 
-     * @return Driver object or null
+     * @return Driver object Optional or empty
      */
     Optional<Driver> findByUsername(String username);
 
     /**
      * Searches for a driver by username and password
      * 
-     * @return Driver object or null
+     * @return Driver object Optional or empty
      */
     Optional<Driver> findByUsernameAndPassword(String username, String password);
 }
