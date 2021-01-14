c4ff1ded4cc7c6a6f1163da9234d707f6aea4c9f
Jason Chatzitheodorou <chatzitheodorou99@gmail.com>
2021-01-14 21:14:37 +0200

    Commented out in index

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Header />
 
-    <ChargingExperience />
+    {/*<ChargingExperience />*/}
     {/*<StationMonitoring />*/}
     {/*<>*/}
   </React.StrictMode>,
